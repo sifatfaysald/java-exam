3fd68fd3c4610fe4be32bb1d8b963fe0df5dd96a
sifatfaysald <sifatfaysald@gmail.com>
2025-07-23 18:20:53 +0600

    implement calculator class with basic arithmetic methods

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Division by zero is not allowed.");
+            return 0;
+        }
+    }
+}
---
+++ MainCalculator.java
@@ -0,0 +1,11 @@
+public class MainCalculator {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Add: " + calc.add(10, 5));
+        System.out.println("Subtract: " + calc.subtract(10, 5));
+        System.out.println("Multiply: " + calc.multiply(10, 5));
+        System.out.println("Divide: " + calc.divide(10, 0));
+        System.out.println("Divide: " + calc.divide(10, 2));
+    }
+}
